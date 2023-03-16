80fed3e63b1d94e52d5eb19c566c03fb8812a5c2
antonkhomenko <antonkhomenko5@gmail.com>
2023-03-16 15:05:41 +0200

    fix imgs

--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -117,7 +117,7 @@ const WorldMap = ({resultTable}) => {
         <>
             <ClickAnimation {...clickAnimation}/>
             <MapWrapper>
-            <MapImg src={'/src/assets/map.png'} alt={'map-img'}/>
+            <MapImg src={'/imgs/map.png'} alt={'map-img'}/>
             {
                 countries.map((item, index) => {
                     return (
