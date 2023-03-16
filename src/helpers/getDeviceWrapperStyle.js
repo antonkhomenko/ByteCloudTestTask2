f7562c7ff495d555eaf54d875526e3a63f007d37
antonkhomenko <antonkhomenko5@gmail.com>
2023-03-16 02:14:09 +0200

    fix device & storage position 1

--- src/helpers/getDeviceWrapperStyle.js
+++ src/helpers/getDeviceWrapperStyle.js
@@ -8,7 +8,11 @@ export const getDeviceWrapperStyle = (location, itemName, deviceAmount) => {
             if(itemName === 'medium') {
                 // styledObj.alignItems = 'flex-end';
                 styledObj.alignSelf = 'end';
-                styledObj.margin = deviceAmount < 3 ? '0 0 0 30%' : '0 0 0 0';
+                // styledObj.margin = deviceAmount < 3 ? '0 0 0 30%' : '0 0 0 0';
+                styledObj.margin = deviceAmount === 3 ? '0 0 0 0' : deviceAmount === 2 ? '0 0 0 30%' : '0 0 0  0';
+            }
+            else if(itemName === 'small') {
+                styledObj.margin =  deviceAmount === 1 && '5% 0 0 65%';
             }
             break;
         }
@@ -20,7 +24,8 @@ export const getDeviceWrapperStyle = (location, itemName, deviceAmount) => {
             else if(itemName === 'small') {
                 styledObj.order = '2';
                 styledObj.alignSelf = 'flex-end';
-                styledObj.margin = deviceAmount < 3 ? '35% 25% 0 0' : '0 23% 10% 0';
+                // styledObj.margin = deviceAmount < 3 ? '35% 25% 0 0' : '0 23% 10% 0';
+                styledObj.margin = deviceAmount === 3 ? '0 23% 10% 0' : deviceAmount === 2 ? '35% 25% 0 0' : '35% 10% 0 0';
             }
             else {
                 styledObj.order = '1';
@@ -33,7 +38,8 @@ export const getDeviceWrapperStyle = (location, itemName, deviceAmount) => {
                 styledObj.order = '2';
                 // styledObj.alignItems = 'center';
                 styledObj.alignSelf = 'center';
-                styledObj.margin = deviceAmount < 3 ? '10% 35% 0 0' : '10% 0 0 15%';
+                // styledObj.margin = deviceAmount < 3 ? '10% 35% 0 0' : '10% 0 0 15%';
+                styledObj.margin = deviceAmount === 3 ? '10% 0 0 15%' : deviceAmount === 2 ? '10% 35% 0 0' : '10% 0 0 42%';
             }
             else if(itemName === 'medium') {
                 styledObj.order = '1';
@@ -53,7 +59,8 @@ export const getDeviceWrapperStyle = (location, itemName, deviceAmount) => {
                 styledObj.order = '2';
                 // styledObj.alignItems = 'center';
                 styledObj.alignSelf = 'center';
-                styledObj.margin = deviceAmount < 3 ? '10% 25% 0 0' : '15% 0 0 26%';
+                // styledObj.margin = deviceAmount < 3 ? '10% 25% 0 0' : '15% 0 0 26%';
+                styledObj.margin = deviceAmount === 3 ? '15% 0 0 26%' : deviceAmount === 2 ? '10% 25% 0 0' : '20% 0 0 55%';
             }
             else if(itemName === 'medium') {
                 styledObj.order = '1';
@@ -75,6 +82,7 @@ export const getDeviceWrapperStyle = (location, itemName, deviceAmount) => {
                 styledObj.order = '3';
                 // styledObj.alignItems = 'flex-end';
                 styledObj.alignSelf = 'end';
+                styledObj.margin = deviceAmount === 1 && '0 0 0 71%';
             }
             else if(itemName === 'medium') {
                 styledObj.order = '1';
